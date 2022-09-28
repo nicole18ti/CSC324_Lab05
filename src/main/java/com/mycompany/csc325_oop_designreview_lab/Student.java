8c41703a6299a45b4840bac3c84b69174a33b2d9
nicolechan1118@gmail.com <>
2022-09-28 10:17:23 -0400

    Finished TODO 1 to 4

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -1,21 +1,29 @@
-
 package com.mycompany.csc325_oop_designreview_lab;
 
-/**
- *
- * @author MoaathAlrajab
- */
-/**
- *
- * @author MoaathAlrajab
- */
-public class Student {
-	// ToDo 1: Make this class a child of Human
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Human { // make this class a child of Human
 
+    public Student(String address, short a) {
+        super(address,a);
+    }
+    private double GPA;
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    //  Add a field for GPA and create setter and getter
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+
+    @Override
+    public String getAddress() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAddress(String address) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
